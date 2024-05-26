d1b3325e37f69c578f86b101b1d414ee5c26d230
rishi <rishi@qburst.com>
2024-05-26 19:45:37 +0530

    fix bug on ref and add use api

--- src/components/formComponent/TestInput.tsx
+++ src/components/formComponent/TestInput.tsx
@@ -1,16 +1,6 @@
-import { forwardRef, useImperativeHandle, useRef } from "react";
+import { RefObject } from "react";
 
-const TestInput = forwardRef<HTMLInputElement>((_props, ref) => {
-  const localRef = useRef<HTMLInputElement>(null);
-  useImperativeHandle(ref, () => localRef.current as HTMLInputElement);
-  return <input type="text" ref={localRef} placeholder="dummy Input" />;
-});
-
+const TestInput = ({ ref }: { ref: RefObject<HTMLInputElement | null> }) => {
+  return <input type="text" ref={ref} placeholder="dummy Input" />;
+};
 export default TestInput;
-
-// import { RefObject } from "react";
-
-// const TestInput = (ref: RefObject<HTMLInputElement | null>) => {
-//   return <input type="text" ref={ref} placeholder="dummy Input" />;
-// };
-// export default TestInput;

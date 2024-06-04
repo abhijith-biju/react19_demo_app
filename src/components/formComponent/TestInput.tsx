7411cb0870792863ec4e2996f9fd26f09f71674d
rishi <rishi@qburst.com>
2024-06-04 09:30:23 +0530

    updated css and ref input

--- src/components/formComponent/TestInput.tsx
+++ src/components/formComponent/TestInput.tsx
@@ -1,9 +1,7 @@
-import { forwardRef, useImperativeHandle, useRef } from "react";
+import { forwardRef } from "react";
 
 const TestInput = forwardRef<HTMLInputElement>((_props, ref) => {
-  const localRef = useRef<HTMLInputElement>(null);
-  useImperativeHandle(ref, () => localRef.current as HTMLInputElement);
-  return <input type="text" ref={localRef} placeholder="dummy Input" />;
+  return <input type="text" ref={ref} placeholder="dummy Input" />;
 });
 
 export default TestInput;

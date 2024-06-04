3dab9039aaf8b4cde4e99a05c934162e7a40d60d
rishi <rishi@qburst.com>
2024-06-04 09:38:22 +0530

    updated test input

--- src/components/formComponent/TestInput.tsx
+++ src/components/formComponent/TestInput.tsx
@@ -5,3 +5,11 @@ const TestInput = forwardRef<HTMLInputElement>((_props, ref) => {
 });
 
 export default TestInput;
+
+// import { RefObject } from "react";
+
+// const TestInput = ({ ref }: { ref: RefObject<HTMLInputElement | null> }) => {
+//   return <input type="text" ref={ref} placeholder="dummy Input" />;
+// };
+
+// export default TestInput;
